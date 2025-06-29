9bfe4f24807d9eb9768d652bf9ed374c223cf392
miz29 <mizaanurrehman128@gmail.com>
2025-06-29 23:43:39 +0530

    amazon linux based ec2

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
     
 }
 resource "aws_instance" "myserver2" {
-    ami = "ami-0f918f7e67a3323f0"
+    ami = "ami-0d03cb826412c6b0f"
     instance_type = "t2.micro"
     tags = {
       Name="SampleServer"
