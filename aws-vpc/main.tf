ec871b6d63e31ff9c557940f0a0bc7ce665feea7
miz29 <mizaanurrehman128@gmail.com>
2025-07-01 23:51:57 +0530

    ec2 in vpc

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -45,7 +45,7 @@ resource "aws_internet_gateway" "my-igw" {
 # route table 
 resource "aws_route_table" "my-rt" {
 vpc_id =aws_vpc.my_vpc.id
-route={
+route{
     cidr_block="0.0.0.0/0"
     gateway_id = aws_internet_gateway.my-igw.id
 } 
@@ -55,4 +55,15 @@ resource "aws_route_table_association" "public-sub" {
 #   associate the route table with subnet
     route_table_id = aws_route_table.my-rt.id
     subnet_id = aws_subnet.public-subnet.id
+}
+
+# creating an ec2 instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public-subnet.id
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
 }
