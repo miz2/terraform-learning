5f67027c65f91060782326fb0c28c09fcc3d18b2
miz29 <mizaanurrehman128@gmail.com>
2025-07-01 23:47:43 +0530

    vpc with terraform

---
+++ aws-vpc/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0"
+    }
+  }
+}
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name="my_vpc"
+    }
+}
+
+# private subnet
+resource "aws_subnet" "private-subnet" {
+  cidr_block = "10.0.1./24"
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "private-subnet"
+  }
+}
+# public subnet
+
+resource "aws_subnet" "public-subnet" {
+  cidr_block = "10.0.1./24"
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# internet gateway 
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+# route table 
+resource "aws_route_table" "my-rt" {
+vpc_id =aws_vpc.my_vpc.id
+route={
+    cidr_block="0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+} 
+}
+
+resource "aws_route_table_association" "public-sub" {
+#   associate the route table with subnet
+    route_table_id = aws_route_table.my-rt.id
+    subnet_id = aws_subnet.public-subnet.id
+}
