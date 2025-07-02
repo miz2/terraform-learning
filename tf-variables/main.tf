abe2bd1068afc4974936b0b43aca47a6a9798902
miz29 <mizaanurrehman128@gmail.com>
2025-07-02 22:56:35 +0530

    variables terraform

---
+++ tf-variables/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+locals {
+  owner = "ABC"
+  name = "MySERVER"
+}
+
+resource "aws_instance" "myserver" {
+  ami           = "ami-0c0e147c706360bd7"
+  instance_type = var.aws_instance_type
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = var.ec2_config.v_size
+    volume_type           = var.ec2_config.v_type
+  }
+
+  tags = merge(var.additional_tags, {
+    Name = local.name
+  })
+}
+
+# can set environment variables which then we do not need to set it in the terminal
